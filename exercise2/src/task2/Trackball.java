d69d609be8a09034e318e01cf234c2f450727b74
Michele Wyss <miwyss@iamexwi.unibe.ch>
2012-10-11 08:04:03 +0200

    trackball works now
    	modified:   exercise2/src/task2/Trackball.java
    	modified:   jrtr/src/jrtr/GLRenderContext.java

--- exercise2/src/task2/Trackball.java
+++ exercise2/src/task2/Trackball.java
@@ -75,7 +75,9 @@ public class Trackball {
 		public void mousePressed(MouseEvent e) {
 			initialVec = projectMousePositionToSphere(e.getX(), e.getY());
 		}
-	    public void mouseReleased(MouseEvent e) {}
+	    public void mouseReleased(MouseEvent e) {
+//	    	newVec = projectMousePositionToSphere(e.getX(), e.getY());
+	    }
 	    public void mouseEntered(MouseEvent e) {}
 	    public void mouseExited(MouseEvent e) {}
 	    public void mouseClicked(MouseEvent e) {}
@@ -114,6 +116,7 @@ public class Trackball {
 			
 			Matrix4f initMatrix = shape.getTransformation();
 			Matrix4f rotMatrix = new Matrix4f();
+			rotMatrix.setIdentity();
 			rotMatrix.setRotation(new AxisAngle4f(rotAxis,angle));
 			
 			initMatrix.mul(rotMatrix,initMatrix);
@@ -128,108 +131,103 @@ public class Trackball {
 	* scene, and starts a timer task to generate an animation.
 	 * @throws IOException 
 	*/
-	public static void main(String[] args) throws IOException
-	{	
-	
-//		shape = new Shape(ObjReader.read("teapot.obj", 1));
+	public static void main(String[] args) throws IOException{	
 
-	VertexData vertexData;
-	vertexData = ObjReader.read("teapot.obj",1);
-	
-	shape = new Shape(vertexData);
-//	shape = makeHouse();
-	
-	sceneManager = new SimpleSceneManager(new Camera(),new Frustum());
-	sceneManager.addShape(shape);
+		shape = new Shape(ObjReader.read("teapot.obj", 2));
 
-	// Make a render panel. The init function of the renderPanel
-	// (see above) will be called back for initialization.
-	renderPanel = new SimpleRenderPanel();
+		sceneManager = new SimpleSceneManager(new Camera(),new Frustum());
+		sceneManager.addShape(shape);
 
-	// Make the main window of this application and add the renderer to it
-	JFrame jframe = new JFrame("simple");
-	jframe.setSize(500, 500);
-	jframe.setLocationRelativeTo(null); // center of screen
-	jframe.getContentPane().add(renderPanel.getCanvas());// put the canvas into a JFrame window
+		// Make a render panel. The init function of the renderPanel
+		// (see above) will be called back for initialization.
+		renderPanel = new SimpleRenderPanel();
 
-	// Add a mouse listener
-	renderPanel.getCanvas().addMouseListener(new SimpleMouseListener());
-	renderPanel.getCanvas().repaint();
-	jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	jframe.setVisible(true); // show window
+		// Make the main window of this application and add the renderer to it
+		JFrame jframe = new JFrame("simple");
+		jframe.setSize(500, 500);
+		jframe.setLocationRelativeTo(null); // center of screen
+		jframe.getContentPane().add(renderPanel.getCanvas());// put the canvas into a JFrame window
+
+		// Add a mouse listener
+		SimpleMouseListener ml = new SimpleMouseListener();
+		renderPanel.getCanvas().addMouseListener(ml);
+		renderPanel.getCanvas().addMouseMotionListener(ml);
+		renderPanel.getCanvas().repaint();
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.setVisible(true); // show window
 	}
 	
-	public static Shape makeHouse()
-	{
-		// A house
-		float vertices[] = {-4,-4,4, 4,-4,4, 4,4,4, -4,4,4,		// front face
-							-4,-4,-4, -4,-4,4, -4,4,4, -4,4,-4, // left face
-							4,-4,-4,-4,-4,-4, -4,4,-4, 4,4,-4,  // back face
-							4,-4,4, 4,-4,-4, 4,4,-4, 4,4,4,		// right face
-							4,4,4, 4,4,-4, -4,4,-4, -4,4,4,		// top face
-							-4,-4,4, -4,-4,-4, 4,-4,-4, 4,-4,4, // bottom face
-
-							-20,-4,20, 20,-4,20, 20,-4,-20, -20,-4,-20, // ground floor
-							-4,4,4, 4,4,4, 0,8,4,				// the roof
-							4,4,4, 4,4,-4, 0,8,-4, 0,8,4,
-							-4,4,4, 0,8,4, 0,8,-4, -4,4,-4,
-							4,4,-4, -4,4,-4, 0,8,-4};
-
-		float normals[] = {0,0,1,  0,0,1,  0,0,1,  0,0,1,		// front face
-						   -1,0,0, -1,0,0, -1,0,0, -1,0,0,		// left face
-						   0,0,-1, 0,0,-1, 0,0,-1, 0,0,-1,		// back face
-						   1,0,0,  1,0,0,  1,0,0,  1,0,0,		// right face
-						   0,1,0,  0,1,0,  0,1,0,  0,1,0,		// top face
-						   0,-1,0, 0,-1,0, 0,-1,0, 0,-1,0,		// bottom face
-
-						   0,1,0,  0,1,0,  0,1,0,  0,1,0,		// ground floor
-						   0,0,1,  0,0,1,  0,0,1,				// front roof
-						   0.707f,0.707f,0, 0.707f,0.707f,0, 0.707f,0.707f,0, 0.707f,0.707f,0, // right roof
-						   -0.707f,0.707f,0, -0.707f,0.707f,0, -0.707f,0.707f,0, -0.707f,0.707f,0, // left roof
-						   0,0,-1, 0,0,-1, 0,0,-1};				// back roof
-						   
-		float colors[] = {1,0,0, 1,0,0, 1,0,0, 1,0,0,
-						  0,1,0, 0,1,0, 0,1,0, 0,1,0,
-						  1,0,0, 1,0,0, 1,0,0, 1,0,0,
-						  0,1,0, 0,1,0, 0,1,0, 0,1,0,
-						  0,0,1, 0,0,1, 0,0,1, 0,0,1,
-						  0,0,1, 0,0,1, 0,0,1, 0,0,1,
-		
-						  0,0.5f,0, 0,0.5f,0, 0,0.5f,0, 0,0.5f,0,			// ground floor
-						  0,0,1, 0,0,1, 0,0,1,							// roof
-						  1,0,0, 1,0,0, 1,0,0, 1,0,0,
-						  0,1,0, 0,1,0, 0,1,0, 0,1,0,
-						  0,0,1, 0,0,1, 0,0,1,};
-
-		// Set up the vertex data
-		VertexData vertexData = new VertexData(42);
-
-		// Specify the elements of the vertex data:
-		// - one element for vertex positions
-		vertexData.addElement(vertices, VertexData.Semantic.POSITION, 3);
-		// - one element for vertex colors
-		vertexData.addElement(colors, VertexData.Semantic.COLOR, 3);
-		// - one element for vertex normals
-		vertexData.addElement(normals, VertexData.Semantic.NORMAL, 3);
-		
-		// The index data that stores the connectivity of the triangles
-		int indices[] = {0,2,3, 0,1,2,			// front face
-						 4,6,7, 4,5,6,			// left face
-						 8,10,11, 8,9,10,		// back face
-						 12,14,15, 12,13,14,	// right face
-						 16,18,19, 16,17,18,	// top face
-						 20,22,23, 20,21,22,	// bottom face
-		                 
-						 24,26,27, 24,25,26,	// ground floor
-						 28,29,30,				// roof
-						 31,33,34, 31,32,33,
-						 35,37,38, 35,36,37,
-						 39,40,41};	
-
-		vertexData.addIndices(indices);
-
-		Shape house = new Shape(vertexData);
-		
-		return house;
-	}
+//	public static Shape makeHouse()
+//	{
+//		// A house
+//		float vertices[] = {-4,-4,4, 4,-4,4, 4,4,4, -4,4,4,		// front face
+//							-4,-4,-4, -4,-4,4, -4,4,4, -4,4,-4, // left face
+//							4,-4,-4,-4,-4,-4, -4,4,-4, 4,4,-4,  // back face
+//							4,-4,4, 4,-4,-4, 4,4,-4, 4,4,4,		// right face
+//							4,4,4, 4,4,-4, -4,4,-4, -4,4,4,		// top face
+//							-4,-4,4, -4,-4,-4, 4,-4,-4, 4,-4,4, // bottom face
+//
+//							-20,-4,20, 20,-4,20, 20,-4,-20, -20,-4,-20, // ground floor
+//							-4,4,4, 4,4,4, 0,8,4,				// the roof
+//							4,4,4, 4,4,-4, 0,8,-4, 0,8,4,
+//							-4,4,4, 0,8,4, 0,8,-4, -4,4,-4,
+//							4,4,-4, -4,4,-4, 0,8,-4};
+//
+//		float normals[] = {0,0,1,  0,0,1,  0,0,1,  0,0,1,		// front face
+//						   -1,0,0, -1,0,0, -1,0,0, -1,0,0,		// left face
+//						   0,0,-1, 0,0,-1, 0,0,-1, 0,0,-1,		// back face
+//						   1,0,0,  1,0,0,  1,0,0,  1,0,0,		// right face
+//						   0,1,0,  0,1,0,  0,1,0,  0,1,0,		// top face
+//						   0,-1,0, 0,-1,0, 0,-1,0, 0,-1,0,		// bottom face
+//
+//						   0,1,0,  0,1,0,  0,1,0,  0,1,0,		// ground floor
+//						   0,0,1,  0,0,1,  0,0,1,				// front roof
+//						   0.707f,0.707f,0, 0.707f,0.707f,0, 0.707f,0.707f,0, 0.707f,0.707f,0, // right roof
+//						   -0.707f,0.707f,0, -0.707f,0.707f,0, -0.707f,0.707f,0, -0.707f,0.707f,0, // left roof
+//						   0,0,-1, 0,0,-1, 0,0,-1};				// back roof
+//						   
+//		float colors[] = {1,0,0, 1,0,0, 1,0,0, 1,0,0,
+//						  0,1,0, 0,1,0, 0,1,0, 0,1,0,
+//						  1,0,0, 1,0,0, 1,0,0, 1,0,0,
+//						  0,1,0, 0,1,0, 0,1,0, 0,1,0,
+//						  0,0,1, 0,0,1, 0,0,1, 0,0,1,
+//						  0,0,1, 0,0,1, 0,0,1, 0,0,1,
+//		
+//						  0,0.5f,0, 0,0.5f,0, 0,0.5f,0, 0,0.5f,0,			// ground floor
+//						  0,0,1, 0,0,1, 0,0,1,							// roof
+//						  1,0,0, 1,0,0, 1,0,0, 1,0,0,
+//						  0,1,0, 0,1,0, 0,1,0, 0,1,0,
+//						  0,0,1, 0,0,1, 0,0,1,};
+//
+//		// Set up the vertex data
+//		VertexData vertexData = new VertexData(42);
+//
+//		// Specify the elements of the vertex data:
+//		// - one element for vertex positions
+//		vertexData.addElement(vertices, VertexData.Semantic.POSITION, 3);
+//		// - one element for vertex colors
+//		vertexData.addElement(colors, VertexData.Semantic.COLOR, 3);
+//		// - one element for vertex normals
+//		vertexData.addElement(normals, VertexData.Semantic.NORMAL, 3);
+//		
+//		// The index data that stores the connectivity of the triangles
+//		int indices[] = {0,2,3, 0,1,2,			// front face
+//						 4,6,7, 4,5,6,			// left face
+//						 8,10,11, 8,9,10,		// back face
+//						 12,14,15, 12,13,14,	// right face
+//						 16,18,19, 16,17,18,	// top face
+//						 20,22,23, 20,21,22,	// bottom face
+//		                 
+//						 24,26,27, 24,25,26,	// ground floor
+//						 28,29,30,				// roof
+//						 31,33,34, 31,32,33,
+//						 35,37,38, 35,36,37,
+//						 39,40,41};	
+//
+//		vertexData.addIndices(indices);
+//
+//		Shape house = new Shape(vertexData);
+//		
+//		return house;
+//	}
 }
