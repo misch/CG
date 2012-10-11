db6e0bac3efec270dd2aa352e219f91e9b6bf8f8
Michele Wyss <michele_wyss@students.unibe.ch>
2012-10-11 09:52:50 +0200

    wrote a simple (stupid) setCornors-Method

--- exercise2/src/task3/FractalLandscape.java
+++ exercise2/src/task3/FractalLandscape.java
@@ -1,5 +1,25 @@
 package task3;
 
 public class FractalLandscape {
+	private int size;
+	private float heights[][];
 	
+	/** The constructor has only one parameter "cornorHeight": all
+	 * four corner will have the same height for now.
+	 * @param size
+	 * @param cornorHeight
+	 */
+	public FractalLandscape(int size, float cornorHeight){
+		this.size = (int)Math.pow(2, size)+1;
+		this.heights = new float[size][size];
+		
+		setUpCornors(cornorHeight);
+	}
+
+	private void setUpCornors(float cornorHeight) {
+		heights[0][0] = cornorHeight;
+		heights[size-1][0] = cornorHeight;
+		heights[0][size-1] = cornorHeight;
+		heights[size-1][size-1] = cornorHeight;
+	}
 }
