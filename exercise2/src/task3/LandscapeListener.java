0afab7303f60bca5dcd3db3e5c62cc229de0a143
Michele Wyss <michele_wyss@students.unibe.ch>
2012-10-18 10:02:03 +0200

    did some refactoring 	new file:   exercise2/src/task3/LandscapeListener.java 	modified:   exercise2/src/task3/ShowLandScape.java

---
+++ exercise2/src/task3/LandscapeListener.java
@@ -0,0 +1,119 @@
+package task3;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import javax.vecmath.AxisAngle4f;
+import javax.vecmath.Matrix4f;
+import javax.vecmath.Vector3f;
+
+import jrtr.Camera;
+import jrtr.RenderPanel;
+
+public class LandscapeListener implements KeyListener, MouseListener, MouseMotionListener {
+		
+		Camera cam;
+		RenderPanel renderPanel;
+		Vector3f goTowards, goFrom;
+		Vector3f camSittingAt, camLookingAt;
+		private int initPosX, initPosY;
+		
+		public LandscapeListener(Camera cam, RenderPanel renderPanel){
+			this.renderPanel = renderPanel;
+			this.cam = cam;
+		}
+		
+		@Override
+		public void keyPressed(KeyEvent e) {
+			switch(e.getKeyChar()) {
+			case 'w':				
+				Vector3f dirZoomIn = cam.getzAxis();
+				dirZoomIn.scale(-1);
+				dirZoomIn.normalize();
+				
+				go(dirZoomIn);
+				break;
+				
+			case 's':
+				Vector3f dirZoomOut = cam.getzAxis();
+				dirZoomOut.normalize();
+				
+				go(dirZoomOut);
+				break;
+				
+			case 'd':
+				Vector3f right = cam.getxAxis();
+				right.normalize();
+				
+				go(right);
+				break;
+				
+			case 'a':
+				Vector3f left = cam.getxAxis();
+				left.scale(-1);
+				left.normalize();
+				
+				go(left);
+				break;	
+			}
+			renderPanel.getCanvas().repaint();
+		}
+
+		private void go(Vector3f direction){
+			camSittingAt = new Vector3f(cam.getCenterOfProjection());
+			camLookingAt = new Vector3f(cam.getLookAtPoint());
+			
+			camSittingAt.add(direction);
+			camLookingAt.add(direction);
+			
+			cam.setCenterOfProjection(camSittingAt);
+			cam.setLookAtPoint(camLookingAt);	
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {}
+
+		@Override
+		public void keyTyped(KeyEvent e) {}
+		
+		public void mousePressed(MouseEvent e) {
+			initPosX = e.getX();
+			initPosY = e.getY();
+		}
+	    public void mouseReleased(MouseEvent e) {}
+	    public void mouseEntered(MouseEvent e) {}
+	    public void mouseExited(MouseEvent e) {}
+	    public void mouseClicked(MouseEvent e) {}
+		public void mouseMoved(MouseEvent arg0) {}
+		
+		public void mouseDragged(MouseEvent e) {
+						
+			Vector3f camLookingAt = new Vector3f(cam.getLookAtPoint());
+			
+			Vector3f pivotTranslation = new Vector3f(cam.getCenterOfProjection());
+			camLookingAt.sub(pivotTranslation);
+			
+			AxisAngle4f axisAngleLeftRight = new AxisAngle4f(cam.getyAxis(), (e.getX()-initPosX)*0.005f);
+			AxisAngle4f axisAngleUpDown = new AxisAngle4f(cam.getxAxis(), (e.getY()-initPosY)*0.005f);
+			
+			Matrix4f rotMatrix = new Matrix4f();
+			rotMatrix.setIdentity();
+			
+			rotMatrix.setRotation(axisAngleLeftRight);			
+			rotMatrix.transform(camLookingAt);
+			
+			rotMatrix.setRotation(axisAngleUpDown);
+			rotMatrix.transform(camLookingAt);
+			
+			camLookingAt.add(pivotTranslation);
+			
+			cam.setLookAtPoint(camLookingAt);
+			
+	    	this.initPosX = e.getX();
+	    	this.initPosY = e.getY();
+	    	renderPanel.getCanvas().repaint();			
+		}		
+	}
--- exercise2/src/task3/ShowLandScape.java
+++ exercise2/src/task3/ShowLandScape.java
@@ -1,14 +1,6 @@
 package task3;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
-
 import javax.swing.JFrame;
-import javax.vecmath.AxisAngle4f;
-import javax.vecmath.Matrix4f;
 import javax.vecmath.Vector3f;
 
 import jrtr.Camera;
@@ -35,144 +27,12 @@ public class ShowLandScape {
 	{
 		/**
 		 * Initialization call-back. We initialize our renderer here.
-		 *
 		 * @param r the render context that is associated with this render panel
 		 */
 		public void init(RenderContext r)
 		{
 			renderContext = r;
 			renderContext.setSceneManager(sceneManager);
-		}		
-	}
-	
-	/**
-	* A timer task that generates an animation. This task triggers
-	* the redrawing of the 3D scene every time it is executed.
-	*/
-	
-	public static class MyKeyListener implements KeyListener {
-		
-		Camera cam;
-		Vector3f goTowards, goFrom;
-		Vector3f camSittingAt, camLookingAt;
-		public MyKeyListener(Camera cam){
-			this.cam = cam;
-		}
-		
-		@Override
-		public void keyPressed(KeyEvent e) {
-			switch(e.getKeyChar()) {
-			case 'w':				
-				Vector3f dirZoomIn = cam.getzAxis();
-				dirZoomIn.scale(-1);
-				dirZoomIn.normalize();
-
-				goFrom = new Vector3f(cam.getCenterOfProjection());
-				goFrom.add(dirZoomIn);
-				cam.setCenterOfProjection(goFrom);
-				
-				camLookingAt = new Vector3f(cam.getLookAtPoint());
-				camLookingAt.add(dirZoomIn);
-				cam.setLookAtPoint(camLookingAt);
-				break;
-				
-			case 's':
-				Vector3f dirZoomOut = cam.getzAxis();
-				dirZoomOut.normalize();
-				
-				goFrom = new Vector3f(cam.getCenterOfProjection());
-				goFrom.add(dirZoomOut);
-				cam.setCenterOfProjection(goFrom);
-				camLookingAt = new Vector3f(cam.getLookAtPoint());
-				camLookingAt.add(dirZoomOut);
-				cam.setLookAtPoint(camLookingAt);
-				break;
-			case 'd':
-				Vector3f dirGoRight = cam.getxAxis();
-				dirGoRight.normalize();
-				
-				camSittingAt = new Vector3f(cam.getCenterOfProjection());
-				camLookingAt = new Vector3f(cam.getLookAtPoint());
-				camSittingAt.add(dirGoRight);
-				camLookingAt.add(dirGoRight);
-				
-				cam.setCenterOfProjection(camSittingAt);
-				cam.setLookAtPoint(camLookingAt);
-				break;
-			case 'a':
-				Vector3f dirGoLeft = cam.getxAxis();
-				dirGoLeft.scale(-1);
-				dirGoLeft.normalize();
-				
-				camSittingAt = new Vector3f(cam.getCenterOfProjection());
-				camLookingAt = new Vector3f(cam.getLookAtPoint());
-				
-				camSittingAt.add(dirGoLeft);
-				camLookingAt.add(dirGoLeft);
-				
-				cam.setCenterOfProjection(camSittingAt);
-				cam.setLookAtPoint(camLookingAt);
-				break;	
-			}
-			renderPanel.getCanvas().repaint();
-		}
-
-		@Override
-		public void keyReleased(KeyEvent e) {}
-
-		@Override
-		public void keyTyped(KeyEvent e) {}
-	}
-	
-	/**
-	* A mouse listener for the main window of this application. This can be
-	* used to process mouse events.
-	*/
-	public static class SimpleMouseListener implements MouseListener, MouseMotionListener
-	{
-	   private Camera cam = new Camera();
-	   private int initPosX, initPosY;
-	   
-	   public SimpleMouseListener(Camera cam){
-		   this.cam = cam;
-	   }
-		
-		public void mousePressed(MouseEvent e) {
-			initPosX = e.getX();
-			initPosY = e.getY();
-		}
-	    public void mouseReleased(MouseEvent e) {}
-	    public void mouseEntered(MouseEvent e) {}
-	    public void mouseExited(MouseEvent e) {}
-	    public void mouseClicked(MouseEvent e) {}
-		public void mouseMoved(MouseEvent arg0) {}
-		
-		public void mouseDragged(MouseEvent e) {
-						
-			Vector3f camLookingAt = new Vector3f(cam.getLookAtPoint());
-			
-			Vector3f pivotTranslation = new Vector3f(cam.getCenterOfProjection());
-			camLookingAt.sub(pivotTranslation);
-			
-			AxisAngle4f axisAngleLeftRight = new AxisAngle4f(cam.getyAxis(), (e.getX()-initPosX)*0.005f);
-			AxisAngle4f axisAngleUpDown = new AxisAngle4f(cam.getxAxis(), (e.getY()-initPosY)*0.005f);
-			
-			Matrix4f rotMatrix = new Matrix4f();
-			rotMatrix.setIdentity();
-			
-			rotMatrix.setRotation(axisAngleLeftRight);			
-			rotMatrix.transform(camLookingAt);
-			
-			rotMatrix.setRotation(axisAngleUpDown);
-			rotMatrix.transform(camLookingAt);
-			
-			camLookingAt.add(pivotTranslation);
-			
-			cam.setLookAtPoint(camLookingAt);
-			
-	    	this.initPosX = e.getX();
-	    	this.initPosY = e.getY();
-	    	renderPanel.getCanvas().repaint();			
 		}		
 	}
 	
@@ -184,23 +44,10 @@ public class ShowLandScape {
 	public static void main(String[] args){	
 
 		int size = 7;
+		int roughness = 5;
+		shape = setUpLandscape(size, roughness);
 
-		float heightTopLeft = 0;
-		float heightBottomLeft = 0;
-		float heightTopRight = 0;
-		float heightBottomRight = 0;
-
-		float[] cornerHeights = new float[4];
-		cornerHeights[0] = heightTopLeft;
-		cornerHeights[1] = heightBottomLeft;
-		cornerHeights[2] = heightTopRight;
-		cornerHeights[3] = heightBottomRight;
-		
-		float roughness= 1;
-
-		shape = new Shape(new FractalLandscape(size, cornerHeights,roughness).getVertexData());
-
-		Camera camera = new Camera(new Vector3f(100,100,200), new Vector3f(((2^size)+1)/2,0,((2^size)+1)/2), new Vector3f(0,1,0));
+		Camera camera = new Camera(new Vector3f(100,100,200), new Vector3f(((2^size)+1)/2,30,((2^size)+1)/2), new Vector3f(0,1,0));
 		Frustum frustum = new Frustum(1,1000,1,(float)(Math.PI/3));
 		
 		sceneManager = new SimpleSceneManager(camera,frustum);
@@ -216,14 +63,32 @@ public class ShowLandScape {
 		jframe.setLocationRelativeTo(null); // center of screen
 		jframe.getContentPane().add(renderPanel.getCanvas());// put the canvas into a JFrame window
 
-		// Add a mouse listener
-		SimpleMouseListener ml = new SimpleMouseListener(camera);
-		renderPanel.getCanvas().addMouseListener(ml);
-		renderPanel.getCanvas().addMouseMotionListener(ml);
-		renderPanel.getCanvas().addKeyListener(new MyKeyListener(camera));
+		// Add a LanscapeListener to use mouse and keys
+		LandscapeListener listener = new LandscapeListener(camera, renderPanel);
+		
+		renderPanel.getCanvas().addMouseListener(listener);
+		renderPanel.getCanvas().addMouseMotionListener(listener);
+		renderPanel.getCanvas().addKeyListener(listener);
 		
 		renderPanel.getCanvas().repaint();
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.setVisible(true); // show window
+	}
+
+	private static Shape setUpLandscape(int size, float roughness) {
+		float heightTopLeft = 0;
+		float heightBottomLeft = 0;
+		float heightTopRight = 0;
+		float heightBottomRight = 0;
+
+		float[] cornerHeights = new float[4];
+		cornerHeights[0] = heightTopLeft;
+		cornerHeights[1] = heightBottomLeft;
+		cornerHeights[2] = heightTopRight;
+		cornerHeights[3] = heightBottomRight;
+		
+		FractalLandscape landscape = new FractalLandscape(size, cornerHeights,roughness);
+		Shape shapeLandscape = new Shape(landscape.getVertexData());
+		return shapeLandscape;
 	}
 }
