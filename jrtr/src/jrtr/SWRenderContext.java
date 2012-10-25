565acad74c0042b3ed375104e38e239488a0b7e6
Michele Wyss <michele_wyss@students.unibe.ch>
2012-10-25 13:45:20 +0200

    tried to clean up code a little (!) bit... 	modified:   jrtr/src/jrtr/SWRenderContext.java

--- jrtr/src/jrtr/SWRenderContext.java
+++ jrtr/src/jrtr/SWRenderContext.java
@@ -6,12 +6,9 @@ import jrtr.VertexData.VertexElement;
 import java.awt.Color;
 import java.awt.Point;
 import java.awt.image.*;
-import java.util.LinkedList;
 
-import javax.vecmath.Color3f;
 import javax.vecmath.Matrix3f;
 import javax.vecmath.Matrix4f;
-import javax.vecmath.Point2f;
 import javax.vecmath.Tuple4f;
 import javax.vecmath.Vector3f;
 import javax.vecmath.Vector4f;
@@ -32,6 +29,7 @@ public class SWRenderContext implements RenderContext {
 	private Matrix4f viewPortMatrix;
 	private Raster clear;
 	private float[][] zBuffer;
+	private int width, height;
 		
 	public void setSceneManager(SceneManagerInterface sceneManager)
 	{
@@ -52,8 +50,7 @@ public class SWRenderContext implements RenderContext {
 		while(iterator.hasNext())
 		{
 			draw(iterator.next());
-		}		
-		
+		}
 		endFrame();
 	}
 
@@ -72,17 +69,14 @@ public class SWRenderContext implements RenderContext {
 	 */
 	public void setViewportSize(int w, int h) // w = width, h = height
 	{
+		this.width = w;
+		this.height = h;
 		this.zBuffer = new float[w][h];
 			
 		viewPortMatrix = new Matrix4f(	w/2f,	0,	0,		w/2f,
 										0,		-h/2f,	0,		h/2f,
 										0,		0,	0.5f,	0.5f,
 										0,		0,	0,		1);
-//		viewPortMatrix.setZero();
-//		viewPortMatrix.setM00(width/2f);
-//		viewPortMatrix.setM11(height/2f);
-//		viewPortMatrix.setM22(1/2f);
-//		viewPortMatrix.setColumn(3, new Vector4f(width/2f, height/2f, 1/2f, 1));
 		
 		colorBuffer = new BufferedImage(w, h, BufferedImage.TYPE_3BYTE_BGR);
 		BufferedImage clearBuffer = new BufferedImage(w,h, BufferedImage.TYPE_3BYTE_BGR);
@@ -121,7 +115,7 @@ public class SWRenderContext implements RenderContext {
 		VertexData vertexData = shape.getVertexData();
 		int indices[] = vertexData.getIndices();
 		Color colors[] = new Color[3];
-		Vector4f positions[] = new Vector4f[3];
+		Vector4f pos[] = new Vector4f[3];
 		Vector4f normals[] = new Vector4f[3];
 		
 		int k = 0; // index of triangle vertex, k is 0,1, or 2
@@ -134,7 +128,7 @@ public class SWRenderContext implements RenderContext {
 				if(vertexElement.getSemantic() == VertexData.Semantic.POSITION){
 					Vector4f p = getPoint(vertexElement, indices[j]);
 					t.transform(p);		
-					positions[k] = p;
+					pos[k] = p;
 					k++;
 				}
 				else if (vertexElement.getSemantic() == VertexData.Semantic.COLOR){
@@ -149,27 +143,16 @@ public class SWRenderContext implements RenderContext {
 				// Draw triangle as soon as we collected the data for 3 vertices
 				if(k == 3)
 				{	
-					Matrix3f edgeFuncCoeff = computeEdgeFuncCoeff(positions);
+					Matrix3f edgeFuncCoeff = computeEdgeFuncCoeff(pos);
 					
-					float w1 = positions[0].w;
-					float w2 = positions[1].w;
-					float w3 = positions[2].w;
-			
-					// Compute the bounding box:
-					Point pixelCoord1 = new Point((int)(positions[0].x/w1), (int)(positions[0].y/w1));
-					Point pixelCoord2= new Point((int)(positions[1].x/w2), (int)(positions[1].y/w2));
-					Point pixelCoord3 = new Point((int)(positions[2].x/w3), (int)(positions[2].y/w3));
-					
-					Point[] boundingBox = getBoundingBox(pixelCoord1, pixelCoord2, pixelCoord3); 
-					
+					Point[] boundingBox = computeBoundingBox(pos);
+					Vector3f wReciprocalValues = new Vector3f(1/pos[0].w, 1/pos[1].w, 1/pos[2].w);
 					
 					for (int x = boundingBox[0].x; x <= boundingBox[1].x;x++){
 						for (int y = boundingBox[0].y; y <= boundingBox[1].y; y++){		
-							
 							Vector3f edgeValues = edgeValues(x,y,edgeFuncCoeff);
-							
-							if (isInTriangle(edgeFuncCoeff, x,y) && pixelIsInWindow(x,y,colorBuffer)){
-								float zBuff = edgeValues.dot(new Vector3f(1/w1, 1/w2, 1/w3))/(edgeValues.x + edgeValues.y + edgeValues.z);
+							if (edgeValues != null){
+								float zBuff = edgeValues.dot(wReciprocalValues)/(edgeValues.x + edgeValues.y + edgeValues.z);
 								if (zBuffer[x][y] < zBuff){
 									zBuffer[x][y] = zBuff;
 									colorBuffer.setRGB(x, y,colors[0].getRGB());
@@ -183,6 +166,33 @@ public class SWRenderContext implements RenderContext {
 		}
 	}
 
+	private Point[] computeBoundingBox(Vector4f[] pos) {
+		Point[] pixelCoord = new Point[3];
+		
+		for (int i = 0; i<3; i++){
+			float w = pos[i].w;
+			pixelCoord[i] = new Point((int)(pos[i].x/w), (int) (pos[i].y/w));
+			
+		}
+		
+		int minX = Math.min(Math.min(pixelCoord[0].x, pixelCoord[1].x), pixelCoord[2].x);
+		int minY = Math.min(Math.min(pixelCoord[0].y, pixelCoord[1].y), pixelCoord[2].y);
+		int maxX = Math.max(Math.max(pixelCoord[0].x, pixelCoord[1].x), pixelCoord[2].x);
+		int maxY = Math.max(Math.max(pixelCoord[0].y, pixelCoord[1].y), pixelCoord[2].y);
+				
+		if (minX < 0)
+			minX = 0;
+		if (minY < 0)
+			minY = 0;
+		if (maxX >= width)
+			maxX = width-1;
+		if (maxY >= height)
+			maxY = height-1;
+		
+		Point[] boundingBox = {new Point(minX,minY), new Point(maxX, maxY)};
+		return boundingBox;	
+	}
+
 	private Vector3f edgeValues(int x, int y, Matrix3f edgeFuncCoeff) {
 		float edgeValues[] = new float[3];
 		Vector3f edgeCoeff = new Vector3f();
@@ -190,24 +200,12 @@ public class SWRenderContext implements RenderContext {
 			edgeFuncCoeff.getColumn(i, edgeCoeff);
 
 			float dotProd = edgeCoeff.dot(new Vector3f(x,y,1));
-			edgeValues[i] = dotProd;
+			if (dotProd < 0)
+				return null;
+			else	
+				edgeValues[i] = dotProd;
 		}
 		return new Vector3f(edgeValues);
-	}
-
-	private Point[] getBoundingBox(Point pixelCoord1, Point pixelCoord2,
-			Point pixelCoord3) {
-		int minX = Math.min(Math.min(pixelCoord1.x, pixelCoord2.x), pixelCoord3.x);
-		int minY = Math.min(Math.min(pixelCoord1.y, pixelCoord2.y), pixelCoord3.y);
-		
-		int maxX = Math.max(Math.max(pixelCoord1.x, pixelCoord2.x), pixelCoord3.x);
-		int maxY = Math.max(Math.max(pixelCoord1.y, pixelCoord2.y), pixelCoord3.y);
-		
-		Point upperLeft = new Point(minX,minY);
-		Point lowerRight = new Point(maxX, maxY);
-		
-		Point[] boundingBox = {upperLeft, lowerRight};
-		return boundingBox;
 	}
 
 	private Vector4f getPoint(VertexElement vertexElement, int j) {
@@ -215,18 +213,6 @@ public class SWRenderContext implements RenderContext {
 		return vec;
 	}
 
-	private boolean isInTriangle(Matrix3f edgeFuncCoeff, int x, int y){
-		for (int i = 0; i<3; i++){
-			Vector3f edgeCoeff = new Vector3f();
-			edgeFuncCoeff.getColumn(i, edgeCoeff);
-			
-			float dotProd = edgeCoeff.dot(new Vector3f(x,y,1));
-			if (dotProd < 0)
-				return false;
-		}
-		return true;
-	}
-	
 	private Matrix3f computeEdgeFuncCoeff(Vector4f[] pos) {
 		Matrix3f coeff = new Matrix3f();
 
@@ -247,15 +233,14 @@ public class SWRenderContext implements RenderContext {
 			// Homogeneous division
 			vec.scale(1/vec.w);
 			
-			if (pixelIsInWindow(vec.x, vec.y, colorBuffer)){
-//			colorBuffer.setRGB((int)(vec.x), colorBuffer.getHeight() - (int)(vec.y)-1, new Color(255,255,255).getRGB());
+			if (pixelIsInWindow(vec.x, vec.y)){
 			colorBuffer.setRGB((int)(vec.x), (int)(vec.y), new Color(255,255,255).getRGB());
 			}
 		}
 	}
 	
-	private boolean pixelIsInWindow(float x, float y, BufferedImage cb){
-		boolean inWindow = (x > 0 && x < cb.getWidth() && y > 0 && y < cb.getHeight());
+	private boolean pixelIsInWindow(float x, float y){
+		boolean inWindow = (x > 0 && x < width && y > 0 && y < height);
 		return inWindow;
 	}
 
