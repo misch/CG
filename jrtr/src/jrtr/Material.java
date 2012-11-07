64b6173a20a969131a20eccd5ce8ba628a3b03bb
Michele Wyss <michele_wyss@students.unibe.ch>
2012-11-07 08:31:09 +0100

    added material properties in a (maybe too) simple way
    modified:   jrtr/src/jrtr/Material.java

--- jrtr/src/jrtr/Material.java
+++ jrtr/src/jrtr/Material.java
@@ -8,13 +8,29 @@ import java.io.IOException;
  */
 public class Material {
 	private SWTexture texture;
+	private Shader shader;
+	private float diffuse;
+	private float specular;
+	private float ambient;
 	
 	public Material(SWTexture texture){
 		this.texture = texture;
 	}
 	
+	public Material(SWTexture texture, Shader shader, float[] properties){
+		this.texture = texture;
+		this.shader = shader;
+		
+		diffuse = properties[0];
+		specular = properties[1];
+		ambient = properties[2];
+	}
 	
 	public SWTexture getTexture(){
 		return texture;
+	}
+	
+	public Shader getShader(){
+		return shader;
 	}
 }
