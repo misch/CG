219f305b5c1ae51d033b8a6f6c23e57d0c3d36db
Michele Wyss <michele_wyss@students.unibe.ch>
2012-11-18 19:13:11 +0100

    implemented some stuff of the next-method (iterator)
    ... There sould be ALL shapes in the stack...?
    
    modified:   jrtr/src/sceneGraph/GraphSceneManager.java

--- jrtr/src/sceneGraph/GraphSceneManager.java
+++ jrtr/src/sceneGraph/GraphSceneManager.java
@@ -4,6 +4,8 @@ import java.util.Iterator;
 import java.util.ListIterator;
 import java.util.Stack;
 
+import javax.vecmath.Matrix4f;
+
 import jrtr.Camera;
 import jrtr.Frustum;
 import jrtr.PointLight;
@@ -61,18 +63,36 @@ public class GraphSceneManager implements SceneManagerInterface {
 		}
 		
 		public RenderItem next()
-		{
-			//TODO
+		{	
 			// If top stack element is a (transformation) group
-			//		pop
-			//		multiply transformation matrix of transformation group
-			//		push children onto stack
-			//
+			if (sceneGraphStack.peek().getChildren() != null){
+				
+				// pop
+				Node node = sceneGraphStack.pop();
+				
+				for (Node child : node.getChildren()){
+					
+					// multiply transformation matrix of transformation group
+					child.getTransformationMatrix().mul(node.getTransformationMatrix(), child.getTransformationMatrix());
+					
+					// push children onto stack
+					sceneGraphStack.push(child);
+				}
+			}
+			
 			// If top stack element is a Leaf
-			//		pop
-			//		multiply its transformation matrix
-			//		return it
-			return null;
+			if (sceneGraphStack.peek().getChildren() == null){
+				
+				// pop
+				Node node = sceneGraphStack.pop();
+				
+				// multiply its transformation matrix
+				Matrix4f shapeMatrix = node.getShape().getTransformation();
+				shapeMatrix.mul(node.getTransformationMatrix(),shapeMatrix);
+			}
+			
+			ShapeNode returnNode = (ShapeNode) sceneGraphStack.pop();
+			return new RenderItem(returnNode.getShape(), returnNode.getShape().getTransformation());
 		}
 	}
 }
