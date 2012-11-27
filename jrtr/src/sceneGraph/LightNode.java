8b9c20aca18a33727014f7c801b6ccf33434742b
Michele Wyss <michele_wyss@students.unibe.ch>
2012-11-27 13:28:19 +0100

    added LightNode-skeleton

---
+++ jrtr/src/sceneGraph/LightNode.java
@@ -0,0 +1,21 @@
+package sceneGraph;
+
+import javax.vecmath.Matrix4f;
+
+import jrtr.Shape;
+
+public class LightNode extends Leaf {
+
+	@Override
+	public Matrix4f getTransformationMatrix() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Shape getShape() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
